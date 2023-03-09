28bdd8dd9ac3ed5b37c97761a511357c03564b89
Ram Rajanbabu <ramr@alleninstitute.org>
2023-03-09 09:49:20 -0800

    Pulling in only 2023 data. Changed sigma to rms.

--- src/generate_rig_noise.py
+++ src/generate_rig_noise.py
@@ -5,7 +5,7 @@ Maintainer: Ramkumar Rajanbabu
 -----------------------------------------------------------------------
 Developer: Ramkumar Rajanbabu
 Date/time created: 10/14/2022
-Description: Template for generating rig-noise.csv
+Description: Template for generating noise_metrics_2022.csv
 -----------------------------------------------------------------------
 """
 
@@ -30,32 +30,38 @@ date_today = dt_today.date() # datetime.date(2022, 10, 21)
 date_prev_day = date_today - timedelta(days=1) # datetime.date(2022, 10, 20)
 
 # Directories
-json_data_dir  = "//allen/programs/celltypes/workgroups/279/Patch-Seq/compiled-jem-data/formatted_data/master_jem.csv"
-noise_data_dir = "C:/Users/ramr/Documents/Github/personal_repos/allen_institute_projects/rig-noise.csv"
+json_data_dir  = "//allen/programs/celltypes/workgroups/279/Patch-Seq/ivscc-data-warehouse/data-sources/jem_lims_metadata.csv"
+noise_data_dir = "//allen/programs/celltypes/workgroups/279/Patch-Seq/ivscc-data-warehouse/data-sources/noise_metrics_2023.csv"
 
 # Lists
 jem_fields = ["jem-date_patch", "jem-date_patch_y", "jem-date_patch_m", "jem-date_patch_d", "jem-id_cell_specimen", "jem-id_patched_cell_container", "jem-status_success_failure"]
-sweep_cols= ["cell_name", "inbath_sigma_long", "inbath_sigma_short", "cellatt_sigma_long", "cellatt_sigma_short", "breakin_sigma_long", "breakin_sigma_short"]
-noise_cols= ["jem-date_patch", "cell_name", "inbath_sigma_long", "inbath_sigma_short", "cellatt_sigma_long", "cellatt_sigma_short", "breakin_sigma_long", "breakin_sigma_short"]
+sweep_cols= ["cell_name", "inbath_long_rms", "inbath_short_rms", "cellatt_long_rms", "cellatt_short_rms", "breakin_long_rms", "breakin_short_rms"]
+noise_cols= ["jem-date_patch", "cell_name", "inbath_long_rms", "inbath_short_rms", "cellatt_long_rms", "cellatt_short_rms", "breakin_long_rms", "breakin_short_rms"]
 
 # Read data source as a pandas dataframe
 jem_df = pd.read_csv(json_data_dir, usecols=jem_fields, low_memory=False)
 # Filters
 jem_df = jem_df.loc[jem_df["jem-status_success_failure"] == "SUCCESS"]
 #jem_df = jem_df.loc[jem_df["jem-status_patch_tube"] == "Patch Tube"]
-jem_df = jem_df.loc[jem_df["jem-date_patch_y"] == 2022]
+jem_df = jem_df.loc[jem_df["jem-date_patch_y"] == 2023]
 # Clean column of duplicates and NAs
 jem_df.drop_duplicates(subset=["jem-id_cell_specimen"], inplace=True)
 jem_df.dropna(subset=["jem-id_cell_specimen"], inplace=True)
+# Sort values by date
+jem_df.sort_values(by=["jem-date_patch"], ascending=True, inplace=True)
 
 # Filters dataframe to user specified date
-jem_df = jem_df[jem_df["jem-date_patch"].str.contains(date_prev_day.strftime("%m/%d/%Y"))]
+#jem_df = jem_df[jem_df["jem-date_patch"].str.contains(date_prev_day.strftime("%m/%d/%Y"))]
 
 # Gather list of experiments based on the filtered pandas dataframe
 cell_list = jem_df["jem-id_cell_specimen"].tolist()
 #len(cell_list)
 
-noise_df = pd.read_csv(noise_data_dir)
+if os.path.exists(noise_data_dir):
+    noise_df = pd.read_csv(noise_data_dir)
+else:
+    noise_df = pd.DataFrame(columns=noise_cols)
+    noise_df.to_csv(noise_data_dir, mode="a", index=False, header=noise_cols)
 
 # Use the 3 voltage sweep names and use startswith (column=stimulus_code)
 vs_inbath_stim_names = ["EXTPINBATH141203", "EXTPINBATH180424"]
@@ -87,12 +93,12 @@ for cell_name in cell_list:
             print(f"{cell_name} does not contain a voltage sweep.")
 
         try: 
-            (inbath_sigma_long, inbath_sigma_short) = calculate_std_vs(vs_inbath_sweep_nums)
-            (cellatt_sigma_long, cellatt_sigma_short) = calculate_std_vs(vs_cellatt_sweep_nums)
-            (breakin_sigma_long, breakin_sigma_short) = calculate_std_vs(vs_breakin_sweep_nums)
+            (inbath_long_rms, inbath_short_rms) = calculate_std_vs(vs_inbath_sweep_nums)
+            (cellatt_long_rms, cellatt_short_rms) = calculate_std_vs(vs_cellatt_sweep_nums)
+            (breakin_long_rms, breakin_short_rms) = calculate_std_vs(vs_breakin_sweep_nums)
         
             sweep_df = pd.DataFrame(columns=sweep_cols)
-            row_list = [cell_name, inbath_sigma_long, inbath_sigma_short, cellatt_sigma_long, cellatt_sigma_short, breakin_sigma_long, breakin_sigma_short]
+            row_list = [cell_name, inbath_long_rms, inbath_short_rms, cellatt_long_rms, cellatt_short_rms, breakin_long_rms, breakin_short_rms]
             row = pd.Series(row_list, index=sweep_df.columns)
             sweep_df = sweep_df.append(row, ignore_index=True)
             
@@ -101,7 +107,7 @@ for cell_name in cell_list:
             
             new_data_df = pd.DataFrame(columns=noise_cols)
             new_data_df = new_data_df.append(df, ignore_index=True)
-            new_data_df.to_csv("rig-noise.csv", mode="a", index=False, header=False)
+            new_data_df.to_csv(noise_data_dir, mode="a", index=False, header=False)
             print()
         
         except (NameError, TypeError) as e:
