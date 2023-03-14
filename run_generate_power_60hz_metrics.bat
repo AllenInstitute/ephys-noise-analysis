1fd418a84d8ad8026d3186ba2f504ea51176c066
Ram Rajanbabu <ramr@alleninstitute.org>
2023-03-14 14:27:33 -0700

    Created run_generate_power_60hz_metrics.bat

---
+++ run_generate_power_60hz_metrics.bat
@@ -0,0 +1,6 @@
+@rem %USERPROFILE% = C:\Users\%USERNAME%
+call %USERPROFILE%\Anaconda3\Scripts\activate.bat
+call activate ephys-noise-analysis-env
+call python src\generate_power_60hz_metrics.py
+call conda deactivate
+@pause
